66eacd78747b62be9da763d6203d8d99f5f92bff
bmaamz <92512826+bmaamz@users.noreply.github.com>
2021-10-14 23:01:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+
+echo "\
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
